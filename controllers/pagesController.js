6b3de431fe42b8682d5c243984ed7fb7ed7d757f
farhanlion <bbss1e3farhan@gmail.com>
2023-09-12 04:44:11 +0800

    mailer set

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -3,11 +3,14 @@ const Video = db.Video;
 
 exports.index = function (params) {
   return async function (req, res) {
-        const videos = await Video.findAll().then()
-        console.log(videos)
-        res.render('index', {
-          cloudinary: params.cloudinary,
-          videos: videos
-        })
-    } 
+    const videos = await Video.findAll({
+      order: [
+        ['createdAt', 'DESC'],
+      ]})
+    res.render('index', {
+      cloudinary: params.cloudinary,
+      videos: videos
+    })
+  }
 }
+
--- controllers/videosController.js
+++ controllers/videosController.js
@@ -1,11 +1,51 @@
+require('dotenv').config();
+var nodemailer = require('nodemailer');
 const db = require("../models");
-const Video = db.Video
+const Video = db.Video;
+
+function sendMail(movements) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS
+    }
+  });
+
+  var smallmovements = 0
+  var bigmovements = 0
+  for (var i = 0;i<movements.length;i++){
+    if (Object.values(movements[i])[1] == 'small'){
+      smallmovements += 1
+    }
+    if (Object.values(movements[i])[1] == 'big'){
+      bigmovements += 1
+    }
+  }
+
+  message = movements.length +" movements detected in total. " + smallmovements + ' small movements were detected and ' + bigmovements+' big movements were detected.'
+
+  var mailOptions = {
+    from: 'farhannation1234@gmail.com',
+    to: 'farhan.ahsan.md@gmail.com',
+    subject: 'MOVEMENT DETECTED',
+    text: 'A movement has been detected in the room. ' + message +' View recording now at http://localhost:8084/'
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+
 
 exports.new = function (params) {
   return async function (req, res) {
         
         console.log(req.body)
-
         const video = Video.build({
           link: req.body.videolink,
           timestamp: req.body.timestamp,
@@ -13,7 +53,8 @@ exports.new = function (params) {
           area: '',
         })
         await video.save()
-
+        
+        sendMail(req.body.movements)
         console.log(video)
 
         res.send({ response: 'received' })
